6720ce863981b04dbca9e1de7b981351e29c1626
chawlid <chawlidyoussef@gmail.com>
2025-05-20 20:46:41 +0100

    push

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import uvicorn
-import Sendpush as sp
-from fastapi import FastAPI
+
+from fastapi import FastAPI, __version__
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+import Sendpush as sp
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Item(BaseModel):
    token: str
@@ -13,16 +14,14 @@ class Item(BaseModel):
    
 @app.post("/token/")
 async def create_token(item: Item):
-    print("Token created")
-   # print(item.token)
-   # print(item.time)
+  
     sp.send_data(item.token, item.time)
     return {"token":  item}
 
 @app.get("/")
 async def root():
-   print("API is connected")
-   return {"message": "API is connected"}
+    return {"message": "Hello World"}
 
-
-
+@app.get('/send')
+async def hello():
+    return {'res': 'send page'}
